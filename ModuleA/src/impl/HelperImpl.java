d5db554872e4ae9149dbd0cb6399ca3057a22d5d
Gh <g21028@eon.com>
2018-06-02 11:26:05 +0300

    Initial commit

---
+++ ModuleA/src/impl/HelperImpl.java
@@ -0,0 +1,10 @@
+package impl;
+
+import util.Helper;
+
+public class HelperImpl implements Helper {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ ModuleA/src/util/Helper.java
@@ -0,0 +1,5 @@
+package util;
+
+public interface Helper {
+    int add(int a, int b);
+}
